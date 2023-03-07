8d56c3e01666f621b3d6d53fabffe6ca462b78c1
Orwa Diraneyya <orwa.diraneyya@wildcodeschool.com>
2023-03-07 15:02:41 +0100

    give back contact object data instead of HTML

--- server.js
+++ server.js
@@ -1,12 +1,17 @@
 // ES6 equivalent: import express from 'express'
 const express = require("express");
+const cors = require('cors');
 const app = express();
+
+app.use(cors());
 
 function rootHandler(_, res) {
   res.send({
     first: "Orwa",
     last: "Diraneyya",
+    avatar: "https://pbs.twimg.com/profile_images/1578722090194173953/lkDUvNAP_400x400.jpg",
     twitter: "@ODiraneyya",
+    notes: "The holiday is coming ⛱️🏖️"
   });
 }
 
